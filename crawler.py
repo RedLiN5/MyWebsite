fc13dd66b7cd7415c4fb0de1c21bf16c5dc843d3
Leslie_Yan <leslieyan0621@gmail.com>
2017-01-28 17:58:34 +0800

    Login page

--- crawler.py
+++ crawler.py
@@ -1,128 +1,43 @@
-#-*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
 
+import time
 import re
-import string
-import sys
 import os
-import warnings
-from bs4 import BeautifulSoup
-import requests
-from lxml import etree
+import sys
+import codecs
+import shutil
+import urllib
 from selenium import webdriver
-import pickle
+from selenium.webdriver.common.keys import Keys
+import selenium.webdriver.support.ui as ui
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = webdriver.Firefox()
+wait = ui.WebDriverWait(driver,10)
+
+#全局变量 文件操作读写信息
+inforead = codecs.open("SinaWeibo_List.txt", 'r', 'utf-8')
+infofile = codecs.open("SinaWeibo_Info.txt", 'a', 'utf-8')
 
 
-def get_cookie_from_network(username, password):
-    url_login = 'http://login.weibo.cn/login/'
-    driver = webdriver.PhantomJS()
-    driver.get(url_login)
-    driver.find_element_by_xpath('//input[@type="text"]').send_keys(username)
-    driver.find_element_by_xpath('//input[@type="password"]').send_keys(password)
-
-    driver.find_element_by_xpath('//input[@type="submit"]').click()
-
-    # 获得 cookie信息
-    cookie_list = driver.get_cookies()
-    print(cookie_list)
-
-    cookie_dict = {}
-    for cookie in cookie_list:
-        #写入文件
-        f = open(cookie['name']+'.weibo','w')
-        pickle.dump(cookie, f)
-        f.close()
-
-        if cookie.has_key('name') and cookie.has_key('value'):
-            cookie_dict[cookie['name']] = cookie['value']
-
-    return cookie_dict
-
-
-
-if sys.version_info[0] == 2:
-    import urllib
-    import urllib2
-elif sys.version_info[0] == 3:
-    import urllib
-else:
-    warnings.warn('Undetected python version.')
-
-# reload(sys)
-# sys.setdefaultencoding('utf-8')
-if(len(sys.argv)>=2):
-    user_id = (int)(sys.argv[1])
-else:
-    user_id = (int)(raw_input(u"请输入user_id: "))
-
-cookie = {"Cookie": "#your cookie"}
-url = 'http://weibo.com/%d?profile_ftype=1&is_all=1'%user_id
-
-html = requests.get(url, cookies = cookie).content
-selector = etree.HTML(html)
-pageNum = (int)(selector.xpath('//input[@name="mp"]')[0].attrib['value'])
-
-result = ""
-urllist_set = set()
-word_count = 1
-image_count = 1
-
-print(u'爬虫准备就绪...')
-
-for page in range(1,pageNum+1):
-
-  #获取lxml页面
-  url = 'http://weibo.cn/u/%d?filter=1&page=%d'%(user_id,page)
-  lxml = requests.get(url, cookies = cookie).content
-
-  #文字爬取
-  selector = etree.HTML(lxml)
-  content = selector.xpath('//span[@class="ctt"]')
-  for each in content:
-    text = each.xpath('string(.)')
-    if word_count>=4:
-      text = "%d :"%(word_count-3) +text+"\n\n"
-    else :
-      text = text+"\n\n"
-    result = result + text
-    word_count += 1
-
-  #图片爬取
-  soup = BeautifulSoup(lxml, "lxml")
-  urllist = soup.find_all('a',href=re.compile(r'^http://weibo.cn/mblog/oripic',re.I))
-  first = 0
-  for imgurl in urllist:
-    urllist_set.add(requests.get(imgurl['href'], cookies = cookie).url)
-    image_count +=1
-
-fo = open("/Users/Personals/%s"%user_id, "wb")
-fo.write(result)
-word_path=os.getcwd()+'/%d'%user_id
-print(u'文字微博爬取完毕')
-
-link = ""
-fo2 = open("/Users/Personals/%s_imageurls"%user_id, "wb")
-for eachlink in urllist_set:
-  link = link + eachlink +"\n"
-fo2.write(link)
-print(u'图片链接爬取完毕')
-
-
-if not urllist_set:
-  print(u'该页面中不存在图片')
-else:
-  #下载图片,保存在当前目录的pythonimg文件夹下
-  image_path=os.getcwd()+'/weibo_image'
-  if os.path.exists(image_path) is False:
-    os.mkdir(image_path)
-  x=1
-  for imgurl in urllist_set:
-    temp= image_path + '/%s.jpg' % x
-    print(u'正在下载第%s张图片' % x)
+def LoginWeibo(username, password):
     try:
-      urllib.urlretrieve(urllib.request.urlopen(imgurl).geturl(),temp)
-    except:
-      print(u"该图片下载失败:%s"%imgurl)
-    x+=1
+        driver.get("http://login.weibo.cn/login/")
+        elem_user = driver.find_element_by_name("mobile")
+        elem_user.send_keys(username)  # 用户名
+        elem_pwd = driver.find_element_by_xpath("/html/body/div[2]/form/div/input[2]")
+        elem_pwd.send_keys(password)  # 密码
 
-print(u'原创微博爬取完毕，共%d条，保存路径%s'%(word_count-4,word_path))
-print(u'微博图片爬取完毕，共%d张，保存路径%s'%(image_count-1,image_path))
+        # 重点: 暂停时间输入验证码
+        # pause(millisenconds)
+
+        time.sleep(20)
+
+        elem_sub = driver.find_element_by_name("submit")
+        elem_sub.click()  # 点击登陆
+        time.sleep(2)
+
+    except Exception as e:
+        print('Error:', e)
+    finally:
+        print(u'End LoginWeibo!\n\n')
