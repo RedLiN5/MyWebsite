23397aec229537044cccfa40140ac6002654aeef
Leslie_Yan <leslieyan0621@gmail.com>
2017-01-31 22:06:13 +0800

    Abbreviate codes

--- validate1.py
+++ validate1.py
@@ -6,35 +6,19 @@ import pyocr.builders
 
 tools = pyocr.get_available_tools()
 if len(tools) == 0:
-    print("No OCR tool found")
-    sys.exit(1)
-# The tools are returned in the recommended order of usage
+    raise ("No OCR tool found")
+
 tool = tools[0]
-print("Will use tool '%s'" % (tool.get_name()))
 
 langs = tool.get_available_languages()
-print("Available languages: %s" % ", ".join(langs))
 lang = langs[0]
 
-
-txt = tool.image_to_string(
-    Image.open('/Users/Leslie/GitHub/WeiboFans/1.png'),
-    lang=lang,
-    builder=pyocr.builders.TextBuilder()
-)
+builder = pyocr.builders.TextBuilder()
 
 
-word_boxes = tool.image_to_string(
-    Image.open('test.png'),
-    lang="eng",
-    builder=pyocr.builders.WordBoxBuilder()
-)
-
-
-line_and_word_boxes = tool.image_to_string(
-    Image.open('test.png'), lang="fra",
-    builder=pyocr.builders.LineBoxBuilder()
-)
+txt = tool.image_to_string(Image.open('/Users/Leslie/GitHub/WeiboFans/1.png'),
+                           lang=lang,
+                           builder=builder)
 
 
 # Digits - Only Tesseract (not 'libtesseract' yet !)
@@ -43,4 +27,14 @@ digits = tool.image_to_string(
     lang=lang,
     builder=pyocr.tesseract.DigitBuilder()
 )
-# digits is a python string
+
+
+if tool.can_detect_orientation():
+    try:
+        orientation = tool.detect_orientation(
+            Image.open('/Users/Leslie/GitHub/WeiboFans/1.png'),
+            lang='fra'
+        )
+        print("Orientation: {}".format(orientation))
+    except pyocr.PyocrException as exc:
+        print("Orientation detection failed: {}".format(exc))
