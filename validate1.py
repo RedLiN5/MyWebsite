5ea05dc92c1c0d8a1377af5e8b308aed8b1deed2
Leslie_Yan <leslieyan0621@gmail.com>
2017-02-03 22:37:08 +0800

    pyocr and tesserocr

--- validate1.py
+++ validate1.py
@@ -1,26 +1,26 @@
 from PIL import Image
-import sys
-
 import pyocr
 import pyocr.builders
+import tesserocr
+from tesserocr import PyTessBaseAPI, PSM
 
 tools = pyocr.get_available_tools()
 if len(tools) == 0:
     raise ("No OCR tool found")
 
 tool = tools[0]
-
 langs = tool.get_available_languages()
 lang = langs[0]
-
 builder = pyocr.builders.TextBuilder()
 
-
-txt = tool.image_to_string(Image.open('/Users/Leslie/GitHub/WeiboFans/1.png'),
+txt = tool.image_to_string(Image.open('/Users/Leslie/GitHub/WeiboFans/1.tif'),
                            lang=lang,
                            builder=builder)
-print(txt)
+print('text:', txt)
 
+
+print(tesserocr.get_languages())
+print('text:', tesserocr.file_to_text('/Users/Leslie/GitHub/WeiboFans/img.jpg'))
 
 
 
@@ -29,7 +29,7 @@ from PIL import Image
 from tesserocr import PyTessBaseAPI, PSM
 
 with PyTessBaseAPI(psm=PSM.AUTO_OSD) as api:
-    image = Image.open("/Users/Leslie/GitHub/WeiboFans/1.png")
+    image = Image.open("/Users/Leslie/GitHub/WeiboFans/1.tif")
     api.SetImage(image)
     api.Recognize()
 
