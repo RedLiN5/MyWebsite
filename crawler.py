208f0c4b70451bdb4efadeed0bd90834559d9ca6
Leslie_Yan <leslieyan0621@gmail.com>
2017-02-05 22:19:15 +0800

    [Feature] Built class

--- crawler.py
+++ crawler.py
@@ -13,52 +13,52 @@ import selenium.webdriver.support.ui as ui
 from pyocr import ocr
 # from validate import *
 
-driver = webdriver.Firefox()
-wait = ui.WebDriverWait(driver,10)
 
 #全局变量 文件操作读写信息
 inforead = codecs.open("SinaWeibo_List.txt", 'r', 'utf-8')
 infofile = codecs.open("SinaWeibo_Info.txt", 'a', 'utf-8')
 
+class SinaWeibo(object):
 
-def LoginWeibo(username, password):
-    try:
-        driver.get("http://login.weibo.cn/login/")
-        elem_user = driver.find_element_by_name("mobile")
-        elem_user.send_keys(username)  # 用户名
-        elem_pwd = driver.find_element_by_xpath("/html/body/div[2]/form/div/input[2]")
-        elem_pwd.send_keys(password)  # 密码
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        wait = ui.WebDriverWait(driver, 10)
 
-        img = driver.find_element_by_xpath('/html/body/div[2]/form/div/img[1]')
-        src = img.get_attribute('src')
-        name = datetime.datetime.now().strftime("%y%m%d%H%M%S")
-        urllib.request.urlretrieve(src, "ValidationImages/" +\
-                                   name +\
-                                   ".jpg")
+    def login(self, username, password):
+        try:
+            self.driver.get("http://login.weibo.cn/login/")
+            elem_user = self.driver.find_element_by_name("mobile")
+            elem_user.send_keys(username)  # 用户名
+            elem_pwd = self.driver.find_element_by_xpath("/html/body/div[2]/form/div/input[2]")
+            elem_pwd.send_keys(password)  # 密码
 
-        # TODO(Leslie): To recognize validation codes.
-        answer = ocr(im = "ValidationImages/" + name + ".jpg")
-        elem_cap = driver.find_element_by_class_name('code')
-        elem_cap.send_keys(answer)
+            img = self.driver.find_element_by_xpath('/html/body/div[2]/form/div/img[1]')
+            src = img.get_attribute('src')
+            name = datetime.datetime.now().strftime("%y%m%d%H%M%S")
+            urllib.request.urlretrieve(src, "ValidationImages/" +\
+                                       name +\
+                                       ".jpg")
 
-        time.sleep(5)
+            # TODO(Leslie): To recognize validation codes.
+            answer = ocr(im = "ValidationImages/" + name + ".jpg")
+            elem_cap = self.driver.find_element_by_name('code')
+            elem_cap.send_keys(answer)
+            time.sleep(3)
 
-        elem_sub = driver.find_element_by_name("submit")
-        elem_sub.click()  # 点击登陆
-        time.sleep(2)
+            elem_remem = self.driver.find_element_by_name('remember')
+            elem_remem.click()
+            elem_sub = self.driver.find_element_by_name("submit")
+            elem_sub.click()  # 点击登陆
+            time.sleep(2)
 
-    except Exception as e:
-        print('Error:', e)
-    finally:
-        print(u'End LoginWeibo!\n\n')
+        except Exception as e:
+            print('Error:', e)
+        finally:
+            print(u'End LoginWeibo!\n\n')
 
-def RetrieveData():
-    pass
+    def RetrieveData(self):
+        pass
 
-def SaveDataset():
-    pass
+    def SaveDataset(self):
+        pass
 
-
-if __name__ == '__main__':
-    LoginWeibo(username='13652063773',
-               password='4372125')
