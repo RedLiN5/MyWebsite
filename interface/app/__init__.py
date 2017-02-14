3dfbd61ac4e5a0a8015f78e74623488a7ea3f628
Leslie_Yan <leslieyan0621@gmail.com>
2017-02-14 17:09:01 +0800

    tutorial

--- interface/app/__init__.py
+++ interface/app/__init__.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return "Hi there, how ya doin'?"
+    return render_template('main.html')
+
+@app.route('/result/')
+def result():
+    return 'hi'
+
 
 if __name__ == '__main__':
     app.run()
