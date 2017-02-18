4cbb003b9c7b5979b77e5f39800f1eddbeeef73b
Leslie_Yan <leslieyan0621@gmail.com>
2017-02-18 22:46:41 +0800

    [Feature] Finish searching likes

--- crawler.py
+++ crawler.py
@@ -3,8 +3,15 @@
 from likes_collect import CollectLikes
 from weibos_collect import CollectWeibo
 
-class SinaWeibo(CollectLikes):
+class SinaWeibo(CollectLikes, CollectWeibo):
 
-    def __init__(self):
+    def __init__(self, username=None, password=None, bloger=None, max_page=None):
         super(CollectLikes, self).__init__()
+        super(SinaWeibo, self).__init__(max_page=max_page)
+        self.username = username
+        self.password = password
+        self.bloger = bloger
 
+    def run(self):
+        self.login(username=self.username, password=self.password)
+        self.search_bloger(bloger=self.bloger)
