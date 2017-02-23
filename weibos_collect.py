e6896fef6fe714c4b256064185865d410b32bfe2
Leslie_Yan <leslieyan0621@gmail.com>
2017-02-23 16:17:58 +0800

    [Debug] Regular exp and string format

--- weibos_collect.py
+++ weibos_collect.py
@@ -19,18 +19,18 @@ from likes_collect import CollectLikes
 class CollectWeibo(CollectLikes):
 
     def __init__(self, max_page=None):
-        super(CollectLikes, self).__init__()
+        super(CollectWeibo, self).__init__()
         self.max_page = max_page
         self.session = requests.Session()
         login_url = "https://passport.weibo.cn/signin/login"
         login_data = {'loginName': self.username, 'loginPassword': self.password}
         r = self.session.post(login_url, data=login_data)
-        self.mycookie = {'Cookie': "SCF=AgtJ6pMqTyS7u12WKayQXv_VFZGcDVVO_7HYTuMX6ACwwIYp7ap0x5ovMiEC1J8GthL08KjyL_ymUS4WEFSCFH4.; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5q0efpquglaB-ZV4xVgDQ-5JpX5o2p5NHD95QE1K.XSo.Neh-7Ws4DqcjTKsH0dsLLPfv9qgRt; _T_WM=b395d1dd6157cb28a2817c7a240b2ef0; SUB=_2A251qDw5DeRxGeRH4lYX-SnMzjyIHXVXU0RxrDV6PUJbkdBeLXf7kW1jKLcuT_PZ8pK_yatHSUoJdUL26A..; SUHB=0RyaarKRUajzb4; SSOLoginState=1487686761; M_WEIBOCN_PARAMS=luicode%3D20000174%26uicode%3D20000174"}
+        self.mycookie = {'Cookie': "SCF=AgtJ6pMqTyS7u12WKayQXv_VFZGcDVVO_7HYTuMX6ACwwIYp7ap0x5ovMiEC1J8GthL08KjyL_ymUS4WEFSCFH4.; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5q0efpquglaB-ZV4xVgDQ-5JpX5o2p5NHD95QE1K.XSo.Neh-7Ws4DqcjTKsH0dsLLPfv9qgRt; _T_WM=b395d1dd6157cb28a2817c7a240b2ef0; SUHB=0RyaarKRUajzb4; SUB=_2A251quLdDeRxGeRH4lYX-SnMzjyIHXVXVI6VrDV6PUJbkdANLVPRkW0QtwhTADSjdCk-WA1PydXES1ECRA..; gsid_CTandWM=4uQA6d841iOYPigpzecnpcz3z0O; PHPSESSID=d8916715880a879aef1fe21ce38ed692"}
 
     def get_weibo(self):
-        m = re.search("/([a-z0-9]+)\?", self.bloger_homepage)
+        m = re.search("/([a-z0-9]+)\?", self.bloger_page)
         user_id = m.group(1)
-        url_front = 'http://weibo.cn/%d?page=' % (user_id)
+        url_front = 'http://weibo.cn/%s?page=' % (user_id)
         columns = ['Date', 'Like', 'Repost', 'Comment']
         # Type includes 'only text', 'photo', 'video' and 'repost'
         page_num = int(1)
@@ -82,7 +82,8 @@ class CollectWeibo(CollectLikes):
                         print(e)
 
             page_num += 1
-        df = pd.DataFrame(data = np.array([pub_dates, likes, reposts, comments]),
+
+        df = pd.DataFrame(data = np.array([pub_dates, likes, reposts, comments]).T,
                           columns = columns)
         file_name = '{0}_weibos.csv'.format(self.bloger)
         exist_files = glob.glob('data/*.csv')
