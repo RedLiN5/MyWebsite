c3758e6c18182f85673db646d379c6c98b0c1626
Leslie_Yan <leslieyan0621@gmail.com>
2017-02-27 20:43:15 +0800

    [Feature] Read data

--- contact_plot.py
+++ contact_plot.py
@@ -1,6 +1,13 @@
 # -*- encoding: utf-8 -*-
+import pandas as pd
+from crawler import SinaWeibo
 
-class ContactPlot(object):
+class ContactPlot(SinaWeibo):
 
     def __init__(self):
-        pass
+        super(ContactPlot, self).__init__()
+
+    def _read_data(self):
+        df = pd.read_table('data/{0}_weibos.csv'%{self.bloger},
+                           sep=',', header=0, index_col=0)
+        return df
