9d0171daa1c5fdb467e569d990fb615b15bae735
Leslie_Yan <leslieyan0621@gmail.com>
2017-02-27 17:46:30 +0800

    Flask main

--- interface/app/__init__.py
+++ interface/app/__init__.py
@@ -3,12 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
-    return render_template('main.html')
+def index():
+    return render_template('index.html')
 
-@app.route('/result/')
-def result():
-    return 'hi'
+@app.route('/profile/<username>')
+def profile(username):
+    return('Hey there, %s!'%(username))
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    return('Hey there, %d!'%(post_id))
 
 
 if __name__ == '__main__':
