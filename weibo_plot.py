42a013adfb968166dcc58d0d0807e32e73fb587f
Leslie_Yan <leslieyan0621@gmail.com>
2017-02-27 20:39:44 +0800

    [Feature] Read data

--- weibo_plot.py
+++ weibo_plot.py
@@ -1,6 +1,19 @@
 # -*- encoding: utf-8 -*-
+import pandas as pd
+from crawler import SinaWeibo
 
-class WeiboPlot(object):
+class WeiboPlot(SinaWeibo):
 
     def __init__(self):
-        pass
+        super(WeiboPlot, self).__init__()
+
+    def _read_data(self):
+        df = pd.read_table('data/{0}_likes.csv'%{self.bloger},
+                           sep=',', header=0, index_col=0)
+        return df
+
+    def _elim_own(self):
+        df = self._read_data()
+        pos = df.Bloger != self.nickname
+        df = df.loc[pos]
+        return df
