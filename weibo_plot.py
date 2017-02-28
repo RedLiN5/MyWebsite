6b3adbe307c03c20e9065f33be086395d2f3ebe0
Leslie_Yan <leslieyan0621@gmail.com>
2017-02-28 16:53:56 +0800

    [Feature] Weibo Records plot

--- weibo_plot.py
+++ weibo_plot.py
@@ -15,23 +15,38 @@ class WeiboPlot(SinaWeibo):
                            sep=',', header=0, index_col=0)
         return df
 
-
-    def preprocess(self):
+    def weibo_trend_plot(self):
         df = self._read_data()
         df['Count'] = pd.Series(np.ones(df.shape[0]),
-                              index=df.index)
-        self.df_count = df[['Date', 'Count']].groupby(['Date']).sum()
-        self.df_count['Date'] = np.array(list(map(lambda x: datetime.strptime(x, "%Y-%m-%d"),
-                                                  self.df_count.index)))
-
-    def weibo_trend_plot(self):
-        self.preprocess()
+                                index=df.index)
+        df_count = df[['Date', 'Count']].groupby(['Date']).sum()
+        df_count['Date'] = np.array(list(map(lambda x: datetime.strptime(x, "%Y-%m-%d"),
+                                             df_count.index)))
         fig = plt.figure(figsize=(20, 9))
         ax = fig.add_subplot(111)
-        ax.plot(self.df_count.Date,
-                self.df_count.Count,
+        ax.plot(df_count.Date,
+                df_count.Count,
                 'c.-')
         ax.title.set_text('Weibo Trend of {0}'%{self.bloger})
         fig.savefig('plots/weibo_trend_{0}.png'%{self.bloger},
                     bbox_inches='tight')
 
+    def weibo_records_plot(self):
+        df = self._read_data()
+        df['Date'] = np.array(list(map(lambda x: datetime.strptime(x, "%Y-%m-%d"),
+                                       df.index)))
+        df_info = df.groupby(['Date']).sum()
+        fig = plt.figure(figsize=(20, 9))
+        ax = fig.add_subplot(111)
+        ax.plot(df_info.index,
+                df_info.Like,
+                'c.-')
+        ax.plot(df_info.index,
+                df_info.Comment,
+                'g.-')
+        ax.plot(df_info.index,
+                df_info.Repost,
+                'm.-')
+        ax.title.set_text('Weibo Records of {0}' % {self.bloger})
+        fig.savefig('plots/weibo_records_{0}.png' % {self.bloger},
+                    bbox_inches='tight')
