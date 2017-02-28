119d00793bf1d457642f896994b0ebe31d3e6004
Leslie_Yan <leslieyan0621@gmail.com>
2017-02-28 11:23:51 +0800

    [Feature] Weibo trend plot

--- weibo_plot.py
+++ weibo_plot.py
@@ -1,6 +1,9 @@
 # -*- encoding: utf-8 -*-
 import pandas as pd
 from crawler import SinaWeibo
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 class WeiboPlot(SinaWeibo):
 
@@ -8,12 +11,27 @@ class WeiboPlot(SinaWeibo):
         super(WeiboPlot, self).__init__()
 
     def _read_data(self):
-        df = pd.read_table('data/{0}_likes.csv'%{self.bloger},
+        df = pd.read_table('data/{0}_weibos.csv'%{self.bloger},
                            sep=',', header=0, index_col=0)
         return df
 
-    def _elim_own(self):
+
+    def preprocess(self):
         df = self._read_data()
-        pos = df.Bloger != self.nickname
-        df = df.loc[pos]
-        return df
+        df['Count'] = pd.Series(np.ones(df.shape[0]),
+                              index=df.index)
+        self.df_count = df[['Date', 'Count']].groupby(['Date']).sum()
+        self.df_count['Date'] = np.array(list(map(lambda x: datetime.strptime(x, "%Y-%m-%d"),
+                                                  self.df_count.index)))
+
+    def weibo_trend_plot(self):
+        self.preprocess()
+        fig = plt.figure(figsize=(20, 9))
+        ax = fig.add_subplot(111)
+        ax.plot(self.df_count.Date,
+                self.df_count.Count,
+                'c.-')
+        ax.title.set_text('Weibo Trend of {0}'%{self.bloger})
+        fig.savefig('plots/weibo_trend_{0}.png'%{self.bloger},
+                    bbox_inches='tight')
+
