8317fd20f8178286a82186e3fdf1aeb970f66c20
Leslie_Yan <leslieyan0621@gmail.com>
2017-03-01 17:58:36 +0800

    [Feature] Get username from HTML

--- interface/app/__init__.py
+++ interface/app/__init__.py
@@ -1,4 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import url_for
+from flask import redirect
+from flask import flash
+import sys
+sys.path.append('/Users/Leslie/GitHub/WeiboFans')
+from sinaweibo import SinaWeibo
 
 app = Flask(__name__)
 
@@ -6,13 +14,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/profile/<username>')
-def profile(username):
-    return('Hey there, %s!'%(username))
 
-@app.route('/post/<int:post_id>')
-def post(post_id):
-    return('Hey there, %d!'%(post_id))
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    error = ''
+    try:
+        if request.method == 'POST':
+            username = request.form['username']
+            print(username)
+            return render_template('/weibo/search_user.html')
+        return render_template('/weibo/search_user.html')
+    except Exception as e:
+        flash(e)
+        return render_template('/weibo/search_user.html', error=error)
+
+
 
 
 if __name__ == '__main__':
