3c2266598259a89a1eb86a355e33e4e421999300
Leslie_Yan <leslieyan0621@gmail.com>
2017-03-02 22:50:40 +0800

    [Fix] Structure

--- sinaweibo.py
+++ sinaweibo.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 
 from weibos_collect import CollectWeibo
+from contact_plot import ContactPlot
+from weibo_plot import WeiboPlot
 
-class SinaWeibo(CollectWeibo):
+class SinaWeibo(CollectWeibo, ContactPlot, WeiboPlot):
 
-    def __init__(self, username=None, password=None, bloger=None, max_page=None):
-        super(SinaWeibo, self).__init__(max_page=max_page)
+    def __init__(self, username=None, password=None, max_page=None):
+        CollectWeibo.__init__(self, username=username,
+                              password=password,
+                              max_page=max_page)
+        ContactPlot.__init__(self)
+        WeiboPlot.__init__(self)
         self.username = username
         self.password = password
-        self.bloger = bloger
 
     def run(self):
-        self.login(username=self.username, password=self.password)
+        self.login()
         self.search_bloger(bloger=self.bloger)
         self.get_weibo()
+        self.start_weibo_plot()
+        self.start_contact_plot()
