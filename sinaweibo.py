ab6749d08de13aa357755ba24920267a9b29a23d
Leslie_Yan <leslieyan0621@gmail.com>
2017-03-03 17:53:30 +0800

    [Fix]

--- sinaweibo.py
+++ sinaweibo.py
@@ -6,10 +6,11 @@ from weibo_plot import WeiboPlot
 
 class SinaWeibo(CollectWeibo, ContactPlot, WeiboPlot):
 
-    def __init__(self, username=None, password=None, max_page=None):
+    def __init__(self, username=None, password=None, bloger=None, max_page=None):
         CollectWeibo.__init__(self, username=username,
                               password=password,
-                              max_page=max_page)
+                              max_page=max_page,
+                              bloger=bloger)
         ContactPlot.__init__(self)
         WeiboPlot.__init__(self)
         self.username = username
@@ -17,7 +18,7 @@ class SinaWeibo(CollectWeibo, ContactPlot, WeiboPlot):
 
     def run(self):
         self.login()
-        self.search_bloger(bloger=self.bloger)
+        self.get_likes()
         self.get_weibo()
         self.start_weibo_plot()
         self.start_contact_plot()
