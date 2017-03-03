2eff9d9568d3afb8b6fb83f585cfbe085e8f421e
Leslie_Yan <leslieyan0621@gmail.com>
2017-03-03 23:21:26 +0800

    [Feature] Save contacts_likes plot

--- contact_plot.py
+++ contact_plot.py
@@ -1,5 +1,11 @@
 # -*- encoding: utf-8 -*-
 import pandas as pd
+from collections import Counter
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+
 
 class ContactPlot(object):
 
@@ -17,3 +23,26 @@ class ContactPlot(object):
         pos = df.Bloger != self.nickname
         df = df.loc[pos]
         return df
+
+    def _sort(self):
+        df = self._elim_own()
+        df_plot = pd.DataFrame({'bloger': list(Counter(df.Bloger).keys()),
+                                'num': list(Counter(df.Bloger).values())})
+        df_plot = df_plot.sort_values(by='num',
+                                      ascending=False).reset_index(drop=True)
+        return df_plot
+
+    def start_contact_plot(self):
+        df_plot = self._sort()
+        upper = max(df_plot.num)
+        mpl.rcParams['font.sans-serif'] = ['SimHei']
+        mpl.rcParams['font.serif'] = ['SimHei']
+        sns.set_style("darkgrid", {"font.sans-serif": ['simhei', 'Arial']})
+        f, ax = plt.subplots(figsize=(6, 15))
+        sns.set_color_codes("pastel")
+        sns.barplot(x="num", y="bloger", data=df_plot,
+                    label="博主的赞", color="b")
+        ax.set(xlim=(0, upper+1), ylabel="",
+               xlabel="点赞总数")
+        f.savefig('plots/weibo_likes_{0}.png' % {self.bloger},
+                  bbox_inches='tight')
