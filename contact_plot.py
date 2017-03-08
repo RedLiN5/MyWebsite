4735ce6e8064333255a1935e733338fafb458d39
Leslie_Yan <leslieyan0621@gmail.com>
2017-03-08 19:09:04 +0800

    [Fix] Remove existing files

--- contact_plot.py
+++ contact_plot.py
@@ -4,7 +4,8 @@ from collections import Counter
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-
+import glob
+import os
 
 
 class ContactPlot(object):
@@ -34,6 +35,12 @@ class ContactPlot(object):
 
     def start_contact_plot(self):
         df_plot = self._sort()
+        file_name = 'weibo_likes_{0}.png' % {self.bloger}
+        exist_files_dir = glob.glob('interface/app/static/plots/*.png')
+        exist_files = list(map(lambda x: x.split('/')[-1], exist_files_dir))
+        if file_name in exist_files:
+            os.remove('interface/app/static/plots/' + file_name)
+
         upper = max(df_plot.num)
         mpl.rcParams['font.sans-serif'] = ['SimHei']
         mpl.rcParams['font.serif'] = ['SimHei']
@@ -44,5 +51,5 @@ class ContactPlot(object):
                     label="博主的赞", color="b")
         ax.set(xlim=(0, upper+1), ylabel="",
                xlabel="点赞总数")
-        f.savefig('plots/weibo_likes_{0}.png' % {self.bloger},
+        f.savefig('interface/app/static/plots/' + file_name,
                   bbox_inches='tight')
