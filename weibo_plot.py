7ece5e535dc2a4961fb27e150cedd49767d70c41
Leslie_Yan <leslieyan0621@gmail.com>
2017-03-09 19:08:37 +0800

    [Fix] Delete existing files and change size in pixel

--- weibo_plot.py
+++ weibo_plot.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import matplotlib.dates as mdates
 from datetime import datetime
 import seaborn as sns
@@ -26,13 +27,25 @@ class WeiboPlot(object):
         df_count = df[['Date', 'Count']].groupby(['Date']).sum()
         df_count['Date'] = np.array(list(map(lambda x: datetime.strptime(x, "%Y-%m-%d"),
                                              df_count.index)))
+
+        file_name = 'weibo_trend_{0}.png'.format(self.bloger)
+        exist_files_dir = glob.glob('interface/app/static/plots/*.png')
+        exist_files = list(map(lambda x: x.split('/')[-1], exist_files_dir))
+        if file_name in exist_files:
+            os.remove('interface/app/static/plots/' + file_name)
+
+        mpl.rcParams['font.sans-serif'] = ['SimHei']
+        mpl.rcParams['font.serif'] = ['SimHei']
+        sns.set_style("white", {"font.sans-serif": ['simhei', 'Arial']})
+
         fig = plt.figure(figsize=(20, 9))
         ax = fig.add_subplot(111)
+        sns.despine()
         ax.plot(df_count.Date,
                 df_count.Count,
                 'c.-')
         ax.title.set_text('Weibo Trend of {0}'.format(self.bloger))
-        fig.savefig('plots/weibo_trend_{0}.png'.format(self.bloger),
+        fig.savefig('interface/app/static/plots/' + file_name,
                     bbox_inches='tight')
 
     def weibo_records_plot(self):
@@ -50,11 +63,12 @@ class WeiboPlot(object):
         table['Date'] = np.array(list(map(lambda x: datetime.strptime(x, "%Y-%m-%d"),
                                           table.Date)))
         table['Subject'] = np.ones(table.shape[0])
-        fig = plt.figure(figsize=(20, 9))
+        fig = plt.figure(figsize=(7, 3), dpi=100)
+        plt.xticks(rotation=35)
         ax = fig.add_subplot(111)
-        sns.set_style("whitegrid")
+        sns.set_style("white")
         sns.tsplot(data=table, time='Date',
-                        condition='Kind', unit='Subject', value='Num')
+                   condition='Kind', unit='Subject', value='Num')
         ax.xaxis.set_major_locator(mdates.AutoDateLocator())
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
         ax.title.set_text('Weibo Records of {0}'.format(self.bloger))
