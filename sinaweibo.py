c7cd0e8766b17666f46faa4b3bfc47423b8caf84
Leslie_Yan <leslieyan0621@gmail.com>
2017-03-13 11:23:03 +0800

    [Fix]

--- sinaweibo.py
+++ sinaweibo.py
@@ -33,17 +33,17 @@ class SinaWeibo(object):
         self.LikesCol = CollectLikes(username=username,
                                      password=password,
                                      bloger=bloger)
-        self.WeiboCol = CollectWeibo(username=username,
-                                     password=password,
-                                     bloger=bloger,
+        self.WeiboCol = CollectWeibo(bloger=bloger,
                                      max_page=max_page)
 
     def start(self):
         self.LikesCol.login()
+        cookie = self.LikesCol.mycookie
         self.LikesCol.get_likes()
         nickname = self.LikesCol.nickname
         bloger_url = self.LikesCol.bloger_page
-        self.WeiboCol.get_weibo(bloger_page=bloger_url,
+        self.WeiboCol.get_weibo(cookie=cookie,
+                                bloger_page=bloger_url,
                                 nickname=nickname)
-        LikesPlot(nickname=nickname).start_likes_plot()
-        WeiboPlot(nickname=nickname).start_weibo_plot()
+        # LikesPlot(nickname=nickname).start_likes_plot()
+        # WeiboPlot(nickname=nickname).start_weibo_plot()
