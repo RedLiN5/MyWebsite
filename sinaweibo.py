a320ba99ea86e01c47b2d00c55f622fe8db2c640
Leslie_Yan <leslieyan0621@gmail.com>
2017-03-14 17:55:02 +0800

    Fix

--- sinaweibo.py
+++ sinaweibo.py
@@ -5,27 +5,6 @@ from weibos_collect import CollectWeibo
 from likes_plot import LikesPlot
 from weibo_plot import WeiboPlot
 
-# class SinaWeibo(CollectWeibo, LikesPlot, WeiboPlot):
-#
-#     def __init__(self, username=None, password=None, bloger=None, max_page=None):
-#         CollectWeibo.__init__(self, username=username,
-#                               password=password,
-#                               max_page=max_page,
-#                               bloger=bloger)
-#         LikesPlot.__init__(self)
-#         WeiboPlot.__init__(self)
-#         self.username = username
-#         self.password = password
-#         self.bloger = bloger
-#
-#     def run(self):
-#         self.login()
-#         self.get_likes()
-#         self.get_weibo()
-#         self.start_weibo_plot()
-#         self.start_likes_plot()
-
-
 class SinaWeibo(object):
 
     def __init__(self, username=None, password=None,
@@ -45,5 +24,5 @@ class SinaWeibo(object):
         self.WeiboCol.get_weibo(cookie=cookie,
                                 bloger_page=bloger_url,
                                 nickname=nickname)
-        # LikesPlot(nickname=nickname).start_likes_plot()
-        # WeiboPlot(nickname=nickname).start_weibo_plot()
+        LikesPlot(nickname=nickname).start_likes_plot()
+        WeiboPlot(nickname=nickname).start_weibo_plot()
