17aad83df5a8f669cd143bf5759df080f4243497
Leslie_Yan <leslieyan0621@gmail.com>
2017-03-16 23:06:11 +0800

    [Fix] Xticks

--- likes_plot.py
+++ likes_plot.py
@@ -50,12 +50,14 @@ class LikesPlot(object):
         f, ax = plt.subplots(figsize=(3, 7), dpi=100)
         frame = plt.gca()
         frame.get_yaxis().set_visible(False)
+        frame.get_xaxis().set_visible(False)
         ax = f.add_subplot(111)
         sns.set_color_codes("pastel")
         sns.barplot(x="num", y="bloger", data=df_plot,
                     label="博主的赞", color="b")
+        ax.set_xticklabels(np.array(list(range(0, upper + 2))))
         sns.despine(top=True, right=True, left=True)
-        ax.set(xlim=(0, upper+1), ylabel="",
+        ax.set(xlim=(0, upper+2), ylabel="",
                xlabel="点赞总数")
         f.savefig('interface/app/static/plots/' + file_name,
                   bbox_inches='tight')
