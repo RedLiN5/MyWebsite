fe975cfc1cf0048ed0564acbca67accc76de3b62
Leslie_Yan <leslieyan0621@gmail.com>
2017-03-18 22:57:35 +0800

    [Fix] Delete useless stuff

--- likes_collect.py
+++ likes_collect.py
@@ -38,19 +38,6 @@ class CollectLikes(object):
             elem_password = self.driver.find_element_by_name('password')
             elem_password.send_keys(self.password)
 
-            # img = self.driver.find_element_by_xpath('/html/body/div[2]/form/div/img[1]')
-            # src = img.get_attribute('src')
-            # name = datetime.datetime.now().strftime("%y%m%d%H%M%S")
-            # urllib.request.urlretrieve(src, "ValidationImages/" +\
-            #                            name +\
-            #                            ".jpg")
-            #
-            # # TODO(Leslie): To recognize validation codes.
-            # answer = ocr(im = "ValidationImages/" + name + ".jpg")
-            # elem_cap = self.driver.find_element_by_name('code')
-            # elem_cap.send_keys(answer)
-            # time.sleep(3)
-
             elem_remem = self.driver.find_element_by_id('login_form_savestate')
             elem_remem.click()
             elem_submit = self.driver.find_element_by_class_name('W_btn_a')
@@ -100,45 +87,6 @@ class CollectLikes(object):
         self.driver.switch_to.window(windows[1])
 
         self._sort_by_likes()
-
-    def search_weibo(self, weibo=None):
-        if not weibo:
-            raise ValueError('"weibo" cannot be empty')
-        elem_search = self.driver.find_element_by_xpath('html/body/div[2]/a[4]')
-        elem_search.click()
-        elem_keyword = self.driver.find_element_by_name('keyword')
-
-        if weibo:
-            elem_keyword.send_keys(weibo)
-            elem_submit = self.driver.find_element_by_name('smblog')
-            elem_submit.click()
-        else:
-            raise ValueError('"weibo" cannot be empty')
-
-    def _following(self):
-        ids = []
-        usernames = []
-        elem_following = self.driver.find_element_by_xpath('html/body/div[2]/div/a[1]')
-        elem_following.click()
-        elem_pagelist = self.driver.find_element_by_id('pagelist')
-        pagelist = elem_pagelist.find_element_by_tag_name('div').text
-        m = re.search("/([0-9]+)页", pagelist)
-        total_page = int(m.group(1))
-
-        for i in range(total_page):
-            for j in range(10):
-                # TODO Count number of tag 'table'
-                elem_user = self.driver.find_element_by_xpath('html/body/table[%d]'%(j+1))
-                elem_user_url = elem_user.find_element_by_xpath('table/tbody/tr/td[2]/a')
-                username = elem_user_url.text
-                url = elem_user_url.get_attribute('href')
-                m = re.search('/([0-9]+)\?', url)
-                id = m.group(1)
-                ids.append(id)
-                usernames.append(username)
-
-    def _friend(self):
-        pass
 
     def _sort_by_likes(self):
         print("Move to bloger's likes")
