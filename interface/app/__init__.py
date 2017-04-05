622484798bd2e4449f017f58f20ee7a9c15fa75e
Leslie_Yan <leslieyan0621@gmail.com>
2017-04-05 17:24:04 +0800

    [Feature] Add Douban search interface

--- interface/app/__init__.py
+++ interface/app/__init__.py
@@ -24,25 +24,48 @@ def projects_demo():
     # TODO (Leslie) Build projects_demo.html
 
 @app.route('/weibo/search', methods=['POST', 'GET'])
-def search():
+def weibo_search():
     error = ''
     try:
         if request.method == 'POST':
             username = request.form['username']
-            print(username == '')
             if username != '':
                 likes_dir = 'plots/weibo_likes_{0}.png'.format(username)
                 trend_dir = 'plots/weibo_trend_{0}.png'.format(username)
                 record_dir = 'plots/weibo_records_{0}.png'.format(username)
-                return render_template('weibo/search_result.html', likes_dir = likes_dir,
-                                       trend_dir= trend_dir, record_dir = record_dir)
+                return render_template('weibo/search_result.html',
+                                       likes_dir = likes_dir,
+                                       trend_dir= trend_dir,
+                                       record_dir = record_dir)
             else:
-                flash('\"Username\" cannot be empty!', category='warning')
+                flash('\"Username\" cannot be empty!',
+                      category='warning')
                 return render_template('weibo/search.html')
         return render_template('weibo/search.html')
     except Exception as e:
         flash(e)
         return render_template('weibo/search.html', error=error)
+
+@app.route('/douban/search', methods=['POST', 'GET'])
+def douban_search():
+    error = ''
+    try:
+        if request.method == 'POST':
+            groupname = request.form['groupname']
+            pagenum = request.form['pagenum']
+            if groupname != '' and pagenum != '':
+                return render_template('weibo/search_result.html',
+                                       groupname=groupname,
+                                       pagenum=pagenum)
+            else:
+                flash('\"Group Name\" or \"Page Num\" may be empty!',
+                      category='warning')
+                return render_template('douban/search.html')
+        return render_template('douban/search.html')
+
+    except Exception as e:
+        flash(e)
+        return render_template('douban/search.html', error=error)
 
 
 if __name__ == '__main__':
