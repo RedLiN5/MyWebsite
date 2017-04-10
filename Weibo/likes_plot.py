77fab4fdf7ce8dc8e553d922b875bb18da19944e
Leslie_Yan <leslieyan0621@gmail.com>
2017-04-10 14:57:27 +0800

    [Debug] If statement when plot a empty graph

--- Weibo/likes_plot.py
+++ Weibo/likes_plot.py
@@ -37,55 +37,63 @@ class LikesPlot(object):
             eval('db.' + self.nickname + '_likes.insert_many(records)')
             return df
         else:
-            file_name = 'weibo_likes_{0}.png'.format(self.nickname)
-            plt.figure()
-            plt.savefig('static/plots/' + file_name,
-                        bbox_inches='tight')
+            return None
 
     def _elim_own(self):
         df = self._read_data()
-        pos = df.Bloger != self.nickname
-        df = df.loc[pos]
+        if isinstance(df, pd.DataFrame):
+            pos = df.Bloger != self.nickname
+            df = df.loc[pos]
         return df
 
     def _sort(self):
         df = self._elim_own()
-        df_plot = pd.DataFrame({'bloger': list(Counter(df.Bloger).keys()),
-                                'num': list(Counter(df.Bloger).values())})
-        df_plot = df_plot.sort_values(by='num',
-                                      ascending=False).reset_index(drop=True)
-        if df_plot.shape[0] <= 30:
-            return df_plot
+        if isinstance(df, pd.DataFrame):
+            df_plot = pd.DataFrame({'bloger': list(Counter(df.Bloger).keys()),
+                                    'num': list(Counter(df.Bloger).values())})
+            df_plot = df_plot.sort_values(by='num',
+                                          ascending=False).reset_index(drop=True)
+            if df_plot.shape[0] <= 30:
+                return df_plot
+            else:
+                return df_plot[:30]
         else:
-            return df_plot[:30]
+            df_plot = None
+            return df_plot
 
     def start_likes_plot(self):
         df_plot = self._sort()
-        file_name = 'weibo_likes_{0}.png'.format(self.nickname)
-        exist_files_dir = glob.glob('static/plots/*.png')
-        print('loc1')
-        exist_files = list(map(lambda x: x.split('/')[-1], exist_files_dir))
-        if file_name in exist_files:
-            print('loc2')
-            os.remove('static/plots/' + file_name)
+        if isinstance(df_plot, pd.DataFrame):
+            file_name = 'weibo_likes_{0}.png'.format(self.nickname)
+            exist_files_dir = glob.glob('static/plots/*.png')
+            print('loc1')
+            exist_files = list(map(lambda x: x.split('/')[-1], exist_files_dir))
+            if file_name in exist_files:
+                print('loc2')
+                os.remove('static/plots/' + file_name)
 
-        upper = max(df_plot.num)
-        mpl.rcParams['font.sans-serif'] = ['SimHei']
-        mpl.rcParams['font.serif'] = ['SimHei']
-        sns.set_style("white", {"font.sans-serif": ['simhei', 'Arial']})
-        f, ax = plt.subplots(figsize=(20, 9), dpi=100)
-        frame = plt.gca()
-        frame.get_yaxis().set_visible(False)
-        frame.get_xaxis().set_visible(False)
-        ax = f.add_subplot(111)
-        sns.set_color_codes("pastel")
-        sns.barplot(x="num", y="bloger", data=df_plot,
-                    label="博主的赞", color="b")
-        ax.set_xticklabels(np.array(list(range(0, upper + 2))))
-        sns.despine(top=True, right=True, left=True)
-        ax.set(xlim=(0, upper+2), ylabel="",
-               xlabel="点赞总数")
-        print('loc3')
-        f.savefig('static/plots/' + file_name,
-                  bbox_inches='tight')
-        print('loc4')
+            upper = max(df_plot.num)
+            mpl.rcParams['font.sans-serif'] = ['SimHei']
+            mpl.rcParams['font.serif'] = ['SimHei']
+            sns.set_style("white", {"font.sans-serif": ['simhei', 'Arial']})
+            f, ax = plt.subplots(figsize=(20, 9), dpi=100)
+            frame = plt.gca()
+            frame.get_yaxis().set_visible(False)
+            frame.get_xaxis().set_visible(False)
+            ax = f.add_subplot(111)
+            sns.set_color_codes("pastel")
+            sns.barplot(x="num", y="bloger", data=df_plot,
+                        label="博主的赞", color="b")
+            ax.set_xticklabels(np.array(list(range(0, upper + 2))))
+            sns.despine(top=True, right=True, left=True)
+            ax.set(xlim=(0, upper+2), ylabel="",
+                   xlabel="点赞总数")
+            print('loc3')
+            f.savefig('static/plots/' + file_name,
+                      bbox_inches='tight')
+            print('loc4')
+        else:
+            file_name = 'weibo_likes_{0}.png'.format(self.nickname)
+            plt.figure()
+            plt.savefig('static/plots/' + file_name,
+                        bbox_inches='tight')
