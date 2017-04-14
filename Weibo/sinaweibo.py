56b982ecb534ab3265830024d8c0b28eb73b4bd6
Leslie_Yan <leslieyan0621@gmail.com>
2017-04-14 11:13:27 +0800

    [Feature] Divide to two parts

--- Weibo/sinaweibo.py
+++ Weibo/sinaweibo.py
@@ -7,19 +7,25 @@ from weibo_plot import WeiboPlot
 
 class SinaWeibo(object):
 
-    def __init__(self, bloger=None, max_page=None):
+    def __init__(self):
+        pass
+
+    def initial(self, bloger=None, max_page=None):
         self.LikesCol = CollectLikes(bloger=bloger)
         self.WeiboCol = CollectWeibo(bloger=bloger,
                                      max_page=max_page)
+        self.captcha_name = self.WeiboCol.captcha_name
 
-    def start(self):
+    def first_part(self):
         self.LikesCol.login()
-        cookie = self.LikesCol.mycookie
         self.LikesCol.get_likes()
-        nickname = self.LikesCol.nickname
-        bloger_url = self.LikesCol.bloger_page
+        self.nickname = self.LikesCol.nickname
+        self.bloger_url = self.LikesCol.bloger_page
+
+    def second_part(self, captcha=None):
+        cookie = self.WeiboCol.get_cookie(captcha=captcha)
         self.WeiboCol.get_weibo(cookie=cookie,
-                                bloger_page=bloger_url,
-                                nickname=nickname)
-        LikesPlot(nickname=nickname).start_likes_plot()
-        WeiboPlot(nickname=nickname).start_weibo_plot()
+                                bloger_page=self.bloger_url,
+                                nickname=self.nickname)
+        LikesPlot(nickname=self.nickname).start_likes_plot()
+        WeiboPlot(nickname=self.nickname).start_weibo_plot()
