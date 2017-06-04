ff732d22c93a98520aae426769b4f07a9cbac0df
Leslie_Yan <leslieyan0621@gmail.com>
2017-06-04 19:33:48 +0800

    [Fix] Change encoding type

--- Amap/crawl_rent.py
+++ Amap/crawl_rent.py
@@ -3,25 +3,25 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import requests
 import csv
-url = "http://sz.58.com/pinpaigongyu/pn/{page}/?minprice=2000_4000"
+url = "http://sh.58.com/pinpaigongyu/pn/{page}/?minprice=4000_5000"
 
 page = 0
 
-csv_file = open("rent.csv","wb") 
+csv_file = open("rent.csv",'w',newline='',encoding='utf-8-sig')
 csv_writer = csv.writer(csv_file, delimiter=',')
 
 while True:
     page += 1
     print("fetch: ", url.format(page=page))
     response = requests.get(url.format(page=page))
-    html = BeautifulSoup(response.text)
+    html = BeautifulSoup(response.text, 'lxml')
     house_list = html.select(".list > li")
 
     if not house_list:
         break
 
     for house in house_list:
-        house_title = house.select("h2")[0].string.encode("utf8")
+        house_title = house.select("h2")[0].string
         house_url = urljoin(url, house.select("a")[0]["href"])
         house_info_list = house_title.split()
 
@@ -30,7 +30,7 @@ while True:
         else:
             house_location = house_info_list[1]
 
-        house_money = house.select(".money")[0].select("b")[0].string.encode("utf8")
+        house_money = house.select(".money")[0].select("b")[0].string
         csv_writer.writerow([house_title, house_location, house_money, house_url])
 
 csv_file.close()
